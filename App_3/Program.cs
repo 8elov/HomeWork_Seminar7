6f8e3627622db49f92b297b77f420ec226e5ce6c
Nikita Belov <lolvvto@yandex.ru>
2022-11-23 20:47:49 +0700

    Add App 3

---
+++ App_3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Запросите от пользователя размерности двумерного массива. 
+// Напишите метод для заполнения массива случайными числами. 
+// Напишите метод для поиска ср. арифметического значения каждого столбца.
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+FillArray(array);
+PrintArray(array);
+
+Console.Write("Введите индекс столбца: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FindAverage(array,num));
+
+double FindAverage(int[,] array, int num)
+{
+    double counter = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        counter = counter + array[i, num];
+    }
+    double average = Math.Round((counter / n),2);
+    return average;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
