c7a54132a6fc5a7ddbdc897f5a307216c5ffcfef
Nikita Belov <lolvvto@yandex.ru>
2022-11-23 17:31:50 +0700

    Add App 1

---
+++ App_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Запросите от пользователя размерности двумерного массива. 
+// Напишите метод для заполнения массива случайными числами. 
+// Напишите метод для вывода массива на экран (постарайтесь вывести массив красиво). 
+// Округлите значения, генерируемые Random до двух знаков после запятой.
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m,n];
+
+FillArray(array);
+PrintArray(array);
+
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = Math.Round(new Random().NextDouble(),2);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
